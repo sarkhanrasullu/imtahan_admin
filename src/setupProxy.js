0dd98bc87ffa2b56323eb037168ffae29bc49d20
Sarkhan Rasullu <sarkhanrasullu@gmail.com>
2019-07-24 13:54:03 +0400

    changes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,12 +1,12 @@
-const proxy = require("http-proxy-middleware");
+// const proxy = require("http-proxy-middleware");
 
-const filter = (pathname, req) => {
-  const result= pathname.match('^/api');
-  return result;
-};
-//http://tripescorestapi.herokuapp.com
-const myproxy = proxy(filter, { target: "http://localhost", changeOrigin: true });
+// const filter = (pathname, req) => {
+//   const result= pathname.match('^/api');
+//   return result;
+// };
+// //http://tripescorestapi.herokuapp.com
+// const myproxy = proxy(filter, { target: "http://localhost", changeOrigin: true });
 
-module.exports = app => {
-  app.use(myproxy);
-};
+// module.exports = app => {
+//   app.use(myproxy);
+// };
