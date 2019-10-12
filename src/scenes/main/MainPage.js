66dac132f87357b8a33a0b348c8187b78c5587b1
Sarkhan Rasullu <bsptechou@gmail.com>
2019-10-12 18:42:25 +0200

    changes

--- src/scenes/main/MainPage.js
+++ src/scenes/main/MainPage.js
@@ -3,9 +3,10 @@ export default class MainPage extends Component {
   render() {
     return (
           <div>
-              <h1>
+              <h1 className="text-center">
                   Admin Panel
               </h1>
+              <h4 class="text-center">Yuxarıdakı uyğun menunu seçib istifadəyə başlaya bilərsiniz.</h4>
           </div>
     );
   }
